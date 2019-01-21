6713722342e79ad8216268247f5e2c3276d82a7a
Leyton Blackler <leytonblackler@gmail.com>
2019-01-22 11:25:58 +1300

    Created and added the toolbar component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import styles from './Layout.module.css'
 
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+
 const layout = (props) => {
   console.log(styles.content);
   return (
     <React.Fragment>
+      <Toolbar />
       <div>Toolbar, SideDrawer, Backdrop</div>
       <main className={styles.content}>
         {props.children}
