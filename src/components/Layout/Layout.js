9bb02d3b72009261b5a52708e2374ce4e580b6a0
Leyton Blackler <leytonblackler@gmail.com>
2019-01-22 12:17:14 +1300

    Created initial side drawer.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import styles from './Layout.module.css'
+import styles from './Layout.module.css';
 
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => {
   console.log(styles.content);
   return (
     <React.Fragment>
       <Toolbar />
+      <SideDrawer />
       <div>Toolbar, SideDrawer, Backdrop</div>
       <main className={styles.content}>
         {props.children}
