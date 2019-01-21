2626446d81033abbbca066bd46cf3bd52ee9b8fa
Leyton Blackler <leytonblackler@gmail.com>
2019-01-22 11:13:14 +1300

    Order summary modal shows total price.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -13,6 +13,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total price: ${props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button type="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button type="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
