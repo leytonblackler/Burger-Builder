2f05c1dfac75df9cad8ca43f708b6fd96c8a5849
Leyton Blackler <leytonblackler@gmail.com>
2019-01-22 11:00:01 +1300

    Clicking the backdrop now closes the modal.

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Backdrop.module.css'
 
 const backdrop = (props) => (
-  props.show ? <div className={styles.Backdrop}></div> : null
+  props.show ? <div className={styles.Backdrop} onClick={props.clicked}></div> : null
 );
 
 export default backdrop;
