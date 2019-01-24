bcf320a3db15e98276e5e4cb221ddc4895257924
Leyton Blackler <leytonblackler@gmail.com>
2019-01-25 10:21:53 +1300

    Shifted layout component into an HOC folder.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(key => {
-    return <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}</li>
-  });
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total price: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button type="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button type="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </React.Fragment>
-  );
+class OrderSummary extends Component {
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(key => {
+      return <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredients[key]}</li>
+    });
+    return (
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total price: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button type="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button type="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </React.Fragment>
+    );
+  }
 }
 
-export default orderSummary;
+export default OrderSummary;
